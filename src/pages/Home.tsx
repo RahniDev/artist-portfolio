0972ffe8a8e78a3c26f7762537ead65ed171646a
RahniDev <rahni32@protonmail.com>
2026-01-07 19:41:54 +0100

    Render collection titles on home page instead

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,22 @@
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { collections } from "../data/artContent";
 
 export default function Home() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const lang = i18n.language as "en" | "fr";
 
   return (
     <section className="home">
       <div className="hero">
         <h1>Sakari</h1>
         <p>{t("heroSubtitle")}</p>
-        <Link to="/gallery" className="cta">View Collections</Link>
+        <div className="collections">
+          {collections.map((col) => (
+            <div key={col.id} className="collection">
+              <h3>{col.title[lang]}</h3>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
