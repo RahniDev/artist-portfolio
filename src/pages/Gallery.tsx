5131ba30dfe3299e083ec7dd32c385ada1253f8d
RahniDev <rahni32@protonmail.com>
2026-01-10 15:25:36 +0100

    Add favicon

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -12,15 +12,17 @@ export default function Gallery() {
   const selectedCollection = collections.find(col => col.id === id);
   if (!selectedCollection) return null;
 
- const images = selectedCollection.pieces.map(p => p.image);
-const titles = selectedCollection.pieces.map(p => p.title);
+  const images = selectedCollection.pieces.map(p => p.image);
+  const titles = selectedCollection.pieces.map(p => p.title);
   const [currentIndex, setCurrentIndex] = useState<number | null>(null);
   return (
     <section className="gallery-page">
       <h2 className="collection-title">
         {selectedCollection.title}
       </h2>
-
+      <p className="series-text"> {lang.startsWith("fr")
+        ? "La série"
+        : "Series"}</p>
       <div className="pieces-grid">
         {selectedCollection.pieces.map((piece, index) => (
           <div key={piece.id} className="piece-card">
@@ -36,19 +38,19 @@ const titles = selectedCollection.pieces.map(p => p.title);
       </div>
 
       {currentIndex !== null && (
-  <ImageModal
-    images={images}
-    titles={titles}
-    currentIndex={currentIndex}
-    onClose={() => setCurrentIndex(null)}
-    onNext={() =>
-      setCurrentIndex(i => Math.min(i! + 1, images.length - 1))
-    }
-    onPrev={() =>
-      setCurrentIndex(i => Math.max(i! - 1, 0))
-    }
-  />
-)}
+        <ImageModal
+          images={images}
+          titles={titles}
+          currentIndex={currentIndex}
+          onClose={() => setCurrentIndex(null)}
+          onNext={() =>
+            setCurrentIndex(i => Math.min(i! + 1, images.length - 1))
+          }
+          onPrev={() =>
+            setCurrentIndex(i => Math.max(i! - 1, 0))
+          }
+        />
+      )}
     </section>
   );
 }
