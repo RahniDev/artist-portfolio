bf61fc049b0cd8108812cbe2006703f57b7c13fc
RahniDev <rahni32@protonmail.com>
2026-07-18 13:57:56 +0200

    Remove fixed img height & add newsletter desc

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -2,6 +2,8 @@ export default function Newsletter() {
   return (
     <>
     <h3 className="newsletter-title">Newsletter</h3>
+    <p>Occasional updates on new works, exhibitions and releases.
+</p>
     <form
       className="newsletter-form"
       action="https://gmail.us3.list-manage.com/subscribe/post?u=c4db4caa3741bf741e810fba9&id=9b6a0b46d5&f_id=003c27e1f0"
